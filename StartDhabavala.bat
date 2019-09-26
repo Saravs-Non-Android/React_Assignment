59c3f932bacc5b21f1bd5b6e1dfbbfc5a1faed33
154773 <saravanan6.kumar6@cognizant.com>
2019-09-26 02:20:21 +0100

    Updated the environment

--- StartDhabavala.bat
+++ StartDhabavala.bat
@@ -1,4 +1,4 @@
-start C:\mongodb\bin\mongod --dbpath C:\mongodb\data\db
+start C:\mongodb\bin\mongod --dbpath C:\data\db
 start C:\mongodb\bin\mongorestore --db dhabaVala --collection restaurants ./db/restaurants.bson
 start C:\mongodb\bin\mongo
 %cd%
